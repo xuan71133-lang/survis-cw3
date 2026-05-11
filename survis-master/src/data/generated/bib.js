b65bacbdd2bf2d4f9d21aa82a6c7a6e07e36805a
xuan71133-lang <xuan71133@gmail.com>
2026-05-11 14:57:41 +0800

    Add files via upload
    Commit message:Add SurVis files

---
+++ survis-master/src/data/generated/bib.js
@@ -0,0 +1,86 @@
+﻿const generatedBibEntries = {
+    "baselinepp2019": {
+        "author": "Chen, W. Y. et al.",
+        "keywords": "hybrid, miniImageNet",
+        "title": "Baseline++",
+        "type": "inproceedings",
+        "year": "2019"
+    },
+    "cifarfs2019": {
+        "author": "Bertinetto, L. et al.",
+        "keywords": "dataset, CIFAR-FS",
+        "title": "CIFAR-FS Benchmark",
+        "type": "article",
+        "year": "2019"
+    },
+    "maml2017": {
+        "author": "Finn, C. et al.",
+        "keywords": "meta-learning, miniImageNet",
+        "title": "Model-Agnostic Meta-Learning",
+        "type": "inproceedings",
+        "year": "2017"
+    },
+    "matchingnet2016": {
+        "author": "Vinyals, O. et al.",
+        "keywords": "metric-based, Omniglot",
+        "title": "Matching Networks for One Shot Learning",
+        "type": "inproceedings",
+        "year": "2016"
+    },
+    "metabaseline2021": {
+        "author": "Chen, Y. et al.",
+        "keywords": "meta-learning, miniImageNet",
+        "title": "Meta-Baseline",
+        "type": "inproceedings",
+        "year": "2021"
+    },
+    "protonet2017": {
+        "author": "Snell, J. and Swersky, K. and Zemel, R.",
+        "keywords": "metric-based, miniImageNet",
+        "title": "Prototypical Networks for Few-shot Learning",
+        "type": "inproceedings",
+        "year": "2017"
+    },
+    "relationnet2018": {
+        "author": "Sung, F. et al.",
+        "keywords": "metric-based, miniImageNet",
+        "title": "Relation Networks for Few-shot Learning",
+        "type": "inproceedings",
+        "year": "2018"
+    },
+    "resnet2016": {
+        "author": "He, K. et al.",
+        "keywords": "feature representation, ImageNet",
+        "title": "Deep Residual Learning for Image Recognition",
+        "type": "inproceedings",
+        "year": "2016"
+    },
+    "simclr2020": {
+        "author": "Chen, T. et al.",
+        "keywords": "feature representation, ImageNet",
+        "title": "SimCLR",
+        "type": "inproceedings",
+        "year": "2020"
+    },
+    "sslprotonet2023": {
+        "author": "Lim, J. Y. et al.",
+        "keywords": "hybrid, CIFAR-FS",
+        "title": "SSL-ProtoNet",
+        "type": "article",
+        "year": "2023"
+    },
+    "survey2013": {
+        "author": "Bengio, Y. et al.",
+        "keywords": "survey",
+        "title": "Representation Learning Survey",
+        "type": "article",
+        "year": "2013"
+    },
+    "vit2021": {
+        "author": "Dosovitskiy, A. et al.",
+        "keywords": "feature representation, ImageNet",
+        "title": "Vision Transformer",
+        "type": "inproceedings",
+        "year": "2021"
+    }
+};
